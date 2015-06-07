39ecce8d978e084bc526c9fa1d7d20dfd51b5671
OceanAirdrop <OceanAirdrop@gmail.com>
2015-06-07 11:14:09 +0100

    initial comment

---
+++ DriveIndexer/DriveIndexer/DriveInfo.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DriveIndexer
+{
+    public static class DriveInfo
+    {
+        const int IdenentSize = 3;
+        
+        public static void ScanDrives()
+        {
+            var sb = new StringBuilder();
+
+            var diskDriveManagement = new ManagementClass("Win32_DiskDrive");
+            var diskDriveInstanceObjectCollection = diskDriveManagement.GetInstances();
+
+            foreach (ManagementObject diskDriveInstanceObject in diskDriveInstanceObjectCollection)
+            {
+                sb.AppendLine(DumpProperties(diskDriveInstanceObject, 0));
+                foreach (ManagementObject diskPartitionObject in diskDriveInstanceObject.GetRelated("Win32_DiskPartition"))
+                {
+                    sb.AppendLine(DumpProperties(diskPartitionObject, 1));
+                    foreach (ManagementBaseObject logicalDiskObject in diskPartitionObject.GetRelated("Win32_LogicalDisk"))
+                    {
+                        sb.AppendLine(DumpProperties(logicalDiskObject, 2));
+                    }
+                }
+            }
+
+            sb.AppendLine();
+            Console.WriteLine(sb.ToString());
+        }
+
+        static String DumpProperties(ManagementBaseObject managementObject, int level)
+        {
+            var padd = "".PadLeft(level * IdenentSize);
+            var sb = new StringBuilder();
+
+            sb.AppendFormat(
+                @"
+                {0}========================================
+                {0}    {1}
+                {0}========================================
+                ",
+            padd, managementObject["Name"]);
+            sb.AppendLine();
+
+            var properties = managementObject.Properties;
+            foreach (var prop in properties)
+            {
+                if (prop != null && prop.Value != null)
+                {
+                    if (prop.IsArray)
+                    {
+                        Array valuelist = prop.Value as Array;
+                        sb.AppendFormat("{0}{1}:", padd, prop.Name);
+                        sb.AppendLine();
+                        foreach (var val in valuelist)
+                        {
+                            sb.AppendFormat("{0}   {1}", padd, val);
+                            sb.AppendLine();
+                        }
+                    }
+                    else
+                    {
+                        sb.AppendFormat("{0}{1}: {2}", padd, prop.Name, prop.Value);
+                        sb.AppendLine();
+                    }
+                }
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ DriveIndexer/DriveIndexer/MainForm.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DriveIndexer
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FileExplorer.ScanAll();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DriveInfo.ScanDrives();
+
+            GetAllDiskDrives();
+        }
+
+        private void GetAllDiskDrives()
+        {
+            var searcher = new ManagementObjectSearcher("SELECT * FROM Win32_DiskDrive");
+
+            foreach (ManagementObject wmi_HD in searcher.Get())
+            {
+                HardDrive hd = new HardDrive();
+                hd.Model = wmi_HD["Model"].ToString();
+                hd.InterfaceType = wmi_HD["InterfaceType"].ToString();
+                hd.Caption = wmi_HD["Caption"].ToString();
+
+                hd.SerialNo = wmi_HD.GetPropertyValue("SerialNumber").ToString();//get the serailNumber of diskdrive
+
+                //hdCollection.Add(hd);
+            }
+
+        }
+
+
+    }
+
+    public class HardDrive
+    {
+        public string Model { get; set; }
+        public string InterfaceType { get; set; }
+        public string Caption { get; set; }
+        public string SerialNo { get; set; }
+    }
+}
